6970c6994a4bf6fd2d744cd766112ee928c415a5
zoercai <zoercai@gmail.com>
2017-07-17 21:07:23 +1200

    Set up project

---
+++ app/src/main/java/com/zoecai/baklavas/AlertZonesFragment.java
@@ -0,0 +1,22 @@
+package com.zoecai.baklavas;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class AlertZonesFragment extends Fragment {
+
+    public AlertZonesFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_alert_zones, container, false);
+    }
+}
